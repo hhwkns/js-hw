e14e33ef3df10353a8dbf12759678b5cc9ca4cc5
HD <hhwkns@gmail.com>
2019-01-25 15:50:51 -0800

    lesson 2 js files

---
+++ lesson-2/canDrive.js
@@ -0,0 +1,8 @@
+let firstName = 'Heather';
+let lastName = 'Dileepan';
+let age = 38
+let x = 16
+
+if (age >= x) {
+    console.log(firstName + ' ' + lastName + ' ' + 'has been driving for'+ ' ' + (age - x) + ' ' + 'years');
+    }
